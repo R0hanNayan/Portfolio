082977ea6768ab57d2c00a5af88ccf63944f155c
Rohan Nayan <rohannayan405@gmail.com>
2024-06-03 15:28:49 +0530

    Added Logo to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import "./navbar.css";
 import Button from '../../utils/Buttons/Button';
+import Logo from '../../utils/Logo/Logo';
 
 const Navbar = () => {
 
-  const items = ["title" ,"description" , "imageURL" , "newsURL"]
   return (
     <div className='nav-container'>
       <Button title={"Home"}/>
       <Button title={"About"}/>
       <Button title={"Services"}/>
+      <Logo />
       <Button title={"Resume"}/>
       <Button title={"Project"}/>
       <Button title={"Contact"}/>
---
+++ src/utils/Logo/Logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./logo.css";
+
+const Logo = () => {
+  return (
+    <div className='logo-container'> 
+        <div className='initials'>
+            <p>ЯN</p>
+        </div>
+        <div className='name'>
+            <p>ROHAN NAYAN</p>
+        </div>
+    </div>
+  )
+}
+
+export default Logo

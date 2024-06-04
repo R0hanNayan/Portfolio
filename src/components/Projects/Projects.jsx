6b545a68cb28ac960e02fde025041a6043337692
Rohan Nayan <rohannayan405@gmail.com>
2024-06-04 14:26:07 +0530

    Added Project Cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import './projects.css';
+import Cards from '../../utils/Cards/Cards';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 const Projects = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1.8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    lazyLoad: true,
+    centerMode: true,
+    focusOnSelect: true,
+  };
+
   return (
     <div className='project-container'>
-        <p className='projects'>My <span className='projects-span'>Projects</span></p>
-        <div className='project-list'>
+      <p className='projects'>My <span className='projects-span'>Projects</span></p>
+      <Slider className='project-list' {...settings}>
+        <div className='card'>
+          <Cards />
         </div>
+        <div className='card'>
+          <Cards />
+        </div>
+        <div className='card'>
+          <Cards />
+        </div>
+      </Slider>
     </div>
   )
 }
---
+++ src/utils/Cards/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './cards.css';
+
+const Cards = () => {
+  return (
+    <div className='card-container'>
+        
+    </div>
+  )
+}
+
+export default Cards
